6b5d8197eb91cb771a34c3bbaa771699a40bff81
Mako108mar <93944348+ICantNameMyselfXbox@users.noreply.github.com>
2026-04-01 00:36:21 +0200

    Add files via upload

--- songs.js
+++ songs.js
@@ -1,99 +1,99 @@
 window.RADIO_SONGS = {
     "Los Santos Rock Radio": {
         "icon": "Images/Radio/LosSantosRockRadio.png",
-        "soundcloud": "https://archive.org/download/radio-for-my-website-mako/Gta%205%20-%20Los%20Santos%20Rock%20Radio%20%28All%20Songs%29.mp3"
+        "soundcloud": "https://archive.org/download/Radio-files-pause-menu/GTA%20V%20%26%20GTA%20Online%20%E2%80%94%20Los%20Santos%20Rock%20Radio%20%EF%BD%9C%20Full%20radio%20station.mp3"
     },
     "Non-Stop-Pop FM": {
         "icon": "Images/Radio/NonStopPopFM.png",
-        "soundcloud": "https://archive.org/download/radio-for-my-website-mako/Gta%205%20NON-STOP%20POP%20radio%20%28all%20songs%29.mp3"
+        "soundcloud": "https://archive.org/download/Radio-files-pause-menu/GTA%20V%20%26%20GTA%20Online%20%E2%80%94%20Non-Stop-Pop%20FM%20%EF%BD%9C%20Full%20radio%20station.mp3"
     },
     "Radio Los Santos": {
         "icon": "Images/Radio/RadioLosSantos.png",
-        "soundcloud": "https://archive.org/download/radio-for-my-website-mako/Radio%20Los%20Santos%20%5BGTA%20V%5D.mp3"
+        "soundcloud": "https://archive.org/download/Radio-files-pause-menu/GTA%20V%20%26%20GTA%20Online%20%E2%80%94%20Radio%20Los%20Santos%20%EF%BD%9C%20Full%20radio%20station.mp3"
     },
     "Channel X": {
         "icon": "Images/Radio/ChannelX.png",
-        "soundcloud": "https://archive.org/download/radio-for-my-website-mako/GTA%20V%20-%20Channel%20X%20Radio%20Station%20%28Full%20Radio%20Station%29.mp3"
+        "soundcloud": "https://archive.org/download/Radio-files-pause-menu/GTA%20V%20%26%20GTA%20Online%20%E2%80%94%20Channel%20X%20%EF%BD%9C%20Full%20radio%20station.mp3"
     },
     "Rebel Radio": {
         "icon": "Images/Radio/RebelRadio.png",
-        "soundcloud": "https://archive.org/download/radio-for-my-website-mako/Rebel%20Radio%20%5BGrand%20Theft%20Auto%20V%5D.mp3"
+        "soundcloud": "https://archive.org/download/Radio-files-pause-menu/GTA%20V%20%26%20GTA%20Online%20%E2%80%94%20Rebel%20Radio%20%EF%BD%9C%20Full%20radio%20station.mp3"
     },
     "Soulwax FM": {
         "icon": "Images/Radio/SoulwaxFM.png",
-        "soundcloud": "https://archive.org/download/radio-for-my-website-mako/Soulwax%20FM%20%5BGTA%20V%5D.mp3"
+        "soundcloud": "https://archive.org/download/Radio-files-pause-menu/GTA%20V%20%26%20GTA%20Online%20%E2%80%94%20Soulwax%20FM%20%EF%BD%9C%20Full%20radio%20station.mp3"
     },
     "East Los FM": {
         "icon": "Images/Radio/EastLosFM.png",
-        "soundcloud": "https://archive.org/download/radio-for-my-website-mako/EAST%20LOS%20FM%20gta%205%20-%20radio%20completa.mp3"
+        "soundcloud": "https://archive.org/download/Radio-files-pause-menu/GTA%20V%20%26%20GTA%20Online%20%E2%80%94%20East%20Los%20FM%20%EF%BD%9C%20Full%20radio%20station.mp3"
     },
     "West Coast Classics": {
         "icon": "Images/Radio/WestCoastClassics.png",
-        "soundcloud": "https://archive.org/download/radio-for-my-website-mako/Radio%20West%20Coast%20Classics%20_%20GTA%20V.mp3"
+        "soundcloud": "https://archive.org/download/Radio-files-pause-menu/GTA%20V%20%26%20GTA%20Online%20%E2%80%94%20West%20Coast%20Classics%20%EF%BD%9C%20Full%20radio%20station.mp3"
     },
     "Blue Ark": {
         "icon": "Images/Radio/TheBlueArk.png",
-        "soundcloud": "https://archive.org/download/radio-for-my-website-mako/Blue%20Ark%20%5BGTA%20V%5D.mp3"
+        "soundcloud": "https://archive.org/download/Radio-files-pause-menu/GTA%20V%20%26%20GTA%20Online%20%E2%80%94%20Blue%20Ark%20%EF%BD%9C%20Full%20radio%20station.mp3"
     },
     "Worldwide FM": {
         "icon": "Images/Radio/WorldwideFM.png",
-        "soundcloud": "https://archive.org/download/radio-for-my-website-mako/WorldWide%20FM%20%5BGTA%20V%5D.mp3"
+        "soundcloud": "https://archive.org/download/Radio-files-pause-menu/GTA%20V%20%26%20GTA%20Online%20%E2%80%94%20Worldwide%20FM%20%EF%BD%9C%20Full%20radio%20station.mp3"
     },
     "FlyLo FM": {
         "icon": "Images/Radio/FlyLoFM.png",
-        "soundcloud": "https://archive.org/download/radio-for-my-website-mako/FlyLo%20FM%20%5BGTA%20V%5D%20ALL%20SONGS__%20%5BJglBzas56qY%5D.mp3"
+        "soundcloud": "https://archive.org/download/Radio-files-pause-menu/GTA%20V%20%26%20GTA%20Online%20%E2%80%94%20FlyLo%20FM%20%EF%BD%9C%20Full%20radio%20station.mp3"
     },
     "The Lowdown 91.1": {
         "icon": "Images/Radio/TheLowdown91.1.png",
-        "soundcloud": "https://archive.org/download/radio-for-my-website-mako/The%20Lowdown%2091.1%20%5BGrand%20Theft%20Auto%20V%5D.mp3"
+        "soundcloud": "https://archive.org/download/Radio-files-pause-menu/GTA%20V%20%26%20GTA%20Online%20%E2%80%94%20The%20Lowdown%2091.1%20%EF%BD%9C%20Full%20radio%20station.mp3"
     },
     "Radio Mirror Park": {
         "icon": "Images/Radio/RadioMirrorPark.png",
-        "soundcloud": "https://archive.org/download/radio-for-my-website-mako/Radio%20Mirror%20Park%20%5BGrand%20Theft%20Auto%20V%5D.mp3"
+        "soundcloud": "https://archive.org/download/Radio-files-pause-menu/GTA%20V%20%26%20GTA%20Online%20%E2%80%94%20Radio%20Mirror%20Park%20%EF%BD%9C%20Full%20radio%20station.mp3"
     },
     "Space 103.2": {
         "icon": "Images/Radio/Space103.2.png",
-        "soundcloud": "https://archive.org/download/radio-for-my-website-mako/Space%20103.2%20%5BGrand%20Theft%20Auto%20V%5D.mp3"
+        "soundcloud": "https://archive.org/download/Radio-files-pause-menu/GTA%20V%20%26%20GTA%20Online%20%E2%80%94%20Space%20103.2%20%EF%BD%9C%20Full%20radio%20station.mp3"
     },
     "Vinewood Boulevard Radio": {
         "icon": "Images/Radio/VinewoodBoulevardRadio.png",
-        "soundcloud": "https://archive.org/download/radio-for-my-website-mako/Vinewood%20Boulevard%20Radio%20%5BGTA%20V%5D%20%5B8dCwPmAPcLw%5D.mp3"
+        "soundcloud": "https://archive.org/download/Radio-files-pause-menu/GTA%20V%20%26%20GTA%20Online%20%E2%80%94%20Vinewood%20Boulevard%20Radio%20%EF%BD%9C%20Full%20radio%20station.mp3"
     },
     "West Coast Talk Radio (WCTR)": {
         "icon": "Images/Radio/WCTR.png",
-        "soundcloud": "https://archive.org/download/radio-for-my-website-mako/WCTR%20%28West%20Coast%20Talk%20Radio%29%20%28GTA%20V%29%20%5BIhCFJnaYvnI%5D.mp3"
+        "soundcloud": null
     },
     "Blaine County Talk Radio": {
         "icon": "Images/Radio/BlaineCountyTalkRadio.png",
-        "soundcloud": "https://archive.org/download/radio-for-my-website-mako/Blaine%20County%20Talk%20Radio%20%5BGTA%20V%5D%20%5BaaXui87cF5Y%5D.mp3"
+        "soundcloud": null
     },
     "The Lab": {
         "icon": "Images/Radio/TheLab.png",
-        "soundcloud": "https://archive.org/download/radio-for-my-website-mako/The%20Lab%20%5BGTA%20V%5D%20%5B4J6JK7ich6E%5D.mp3"
+        "soundcloud": "https://archive.org/download/Radio-files-pause-menu/GTA%20V%20%26%20GTA%20Online%20%E2%80%94%20The%20Lab%20%EF%BD%9C%20Full%20radio%20station.mp3"
     },
     "blonded Los Santos 97.8 FM": {
         "icon": "Images/Radio/BlondedLosSantos97.8FM.png",
-        "soundcloud": "https://archive.org/download/radio-for-my-website-mako/blonded%20Los%20Santos%2097.8%20FM%20%5BGTA%20V%5D%20%5BXNxUKtODPDs%5D.mp3"
+        "soundcloud": "https://archive.org/download/Radio-files-pause-menu/GTA%20V%20%26%20GTA%20Online%20%E2%80%94%20blonded%20Los%20Santos%2097.8%20FM%20%EF%BD%9C%20Full%20radio%20station.mp3"
     },
     "iFruit Radio": {
         "icon": "Images/Radio/IFruitRadio.png",
-        "soundcloud": "https://archive.org/download/radio-for-my-website-mako/GTA%20V%20%26%20GTA%20Online%20%E2%80%94%20iFruit%20Radio%20_%20Full%20radio%20station%20%5BfpvJaphZ2_g%5D.mp3"
+        "soundcloud": "https://archive.org/download/Radio-files-pause-menu/GTA%20V%20%26%20GTA%20Online%20%E2%80%94%20iFruit%20Radio%20%EF%BD%9C%20Full%20radio%20station.mp3"
     },
     "The Music Locker": {
         "icon": "Images/Radio/90px-MusicLockerRadio.png",
-        "soundcloud": null
+        "soundcloud": "https://archive.org/download/Radio-files-pause-menu/GTA%20Online%20%E2%80%94%20Music%20Locker%20Radio%20%EF%BD%9C%20Full%20radio%20station.mp3"
     },
     "Kult FM": {
         "icon": "Images/Radio/KultFM.png",
-        "soundcloud": "https://archive.org/download/radio-for-my-website-mako/K.U.L.T.%2099.1%20FM%20%5BGTA%20Online_%20The%20Cayo%20Perico%20Update%5D%20ALL%20SONGS__%20%5BFY9EiOllRhE%5D.mp3"
+        "soundcloud": "https://archive.org/download/Radio-files-pause-menu/GTA%20Online%20%E2%80%94%20Kult%20FM%20%EF%BD%9C%20Full%20radio%20station.mp3"
     },
     "LSUR": {
         "icon": "Images/Radio/LSUR.png",
-        "soundcloud": null
+        "soundcloud": "https://archive.org/download/Radio-files-pause-menu/GTA%20V%20%26%20GTA%20Online%20%E2%80%94%20Los%20Santos%20Underground%20Radio%20%EF%BD%9C%20Full%20radio%20station.mp3"
     },
     "MOTOMAMI Los Santos": {
         "icon": "Images/Radio/MOTOMAMILosSantos.png",
-        "soundcloud": "https://archive.org/download/radio-for-my-website-mako/MOTOMAMI%20Los%20Santos_%20Radio%20%5BxNjHCRa-pFk%5D.mp3"
+        "soundcloud": "https://archive.org/download/Radio-files-pause-menu/GTA%20Online%20%E2%80%94%20MOTOMAMI%20Los%20Santos%20%EF%BD%9C%20Full%20radio%20station.mp3"
     },
     "Self Radio": {
         "icon": "Images/Radio/SelfRadio.png",
@@ -101,6 +101,6 @@ window.RADIO_SONGS = {
     },
     "Still Slipping LS": {
         "icon": "Images/Radio/StillSlippingLS.png",
-        "soundcloud": "https://archive.org/download/radio-for-my-website-mako/Still%20Slipping%20Los%20Santos%20%28Hosted%20by%20DJ%20Joy%20Orbison%29%20%5BGTA%20V%5D%20W_%20TIMESTAMPS-%20Cayo%20Perico%20Heist%20%5BFwbNs4oMmF8%5D.mp3"
+        "soundcloud": "https://archive.org/download/Radio-files-pause-menu/GTA%20Online%20%E2%80%94%20Still%20Slipping%20Los%20Santos%20%EF%BD%9C%20Full%20radio%20station.mp3"
     }
 };
